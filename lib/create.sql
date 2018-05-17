c2c2cdce187e6cf144b0496dd09a85c21dd693da
Suchitra Naik <susanaik@gmail.com>
2018-05-17 14:00:16 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,8 +1,15 @@
 CREATE TABLE  projects (
   id INTEGER PRIMARY KEY,
-  title TEXT
-  category TEXT
-  funding_goal INTEGER
-  start_date DATE
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
   end_date DATE
+);
+
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT, 
+  age INTEGER
 );
