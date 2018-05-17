2bbdd4e70c3382047b93c13705df1ebcaf3df3f4
Suchitra Naik <susanaik@gmail.com>
2018-05-17 15:04:51 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "Write your SQL query Here"
-"select projects.category, pledges.amount from projects, pledges where projects.category='music'"
+"select projects.category, pledges.amount from projects, pledges where projects.category='music' AND projects.id = pledges.project_id"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
